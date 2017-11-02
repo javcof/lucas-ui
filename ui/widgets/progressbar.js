b1129501e8702ac18d235db06d199b0f47de65ac
javcof <javcof@qq.com>
2017-11-02 17:22:45 +0800

    Add widget progressbar

--- ui/widgets/progressbar.js
+++ ui/widgets/progressbar.js
@@ -1,12 +1,41 @@
 $.widget("ui.progressbar", {
 	
 	options: {
-		value: 0
+		value: 0,
+		min: 0,
+		max: 100,
+		complete: null
+	},
+	
+	value: function(val) {
+		if (val === undefined) {
+			return this.options.value;
+		}
+		this.options.value = this._constraint(val);
+		this._refresh();
 	},
 	
 	_create: function() {
 		this.element.addClass("ui-progressbar ui-widget ui-widget-content");
-		$("<div>").appendTo(this.element).addClass("ui-progressbar-value ui-widget-header").width(this.options.value + "%");
+		this.label = $("<div>").appendTo(this.element).addClass("ui-progressbar-value ui-widget-header");
+		this._refresh();
+	},
+	
+	_refresh: function() {
+		this.label.width(this.options.value + "%");
+		if (this.options.value === this.options.max) {
+			this._trigger("complete");
+		}
+	},
+	
+	_constraint: function(val) {
+		if (val < this.options.min) {
+			return this.options.min;
+		}
+		if (val > this.options.max) {
+			return this.options.max;
+		}
+		return val;
 	}
 	
 });
