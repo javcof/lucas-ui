6bdd4a3fd9ef96e986b76113e06c12f222a822e4
javcof <javcof@qq.com>
2017-11-09 23:34:45 +0800

    Add progressbar demo

--- ui/widgets/progressbar.js
+++ ui/widgets/progressbar.js
@@ -1,12 +1,12 @@
 $.widget("ui.progressbar", {
-	
+
 	options: {
 		value: 0,
 		min: 0,
 		max: 100,
 		complete: null
 	},
-	
+
 	value: function(val) {
 		if (val === undefined) {
 			return this.options.value;
@@ -14,20 +14,20 @@ $.widget("ui.progressbar", {
 		this.options.value = this._constraint(val);
 		this._refresh();
 	},
-	
+
 	_create: function() {
 		this.element.addClass("ui-progressbar ui-widget ui-widget-content");
 		this.label = $("<div>").appendTo(this.element).addClass("ui-progressbar-value ui-widget-header");
 		this._refresh();
 	},
-	
+
 	_refresh: function() {
 		this.label.width(this.options.value + "%");
 		if (this.options.value === this.options.max) {
 			this._trigger("complete");
 		}
 	},
-	
+
 	_constraint: function(val) {
 		if (val < this.options.min) {
 			return this.options.min;
@@ -37,5 +37,5 @@ $.widget("ui.progressbar", {
 		}
 		return val;
 	}
-	
+
 });
